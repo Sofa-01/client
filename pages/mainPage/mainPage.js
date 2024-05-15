cd7a3d305ed9f2b6f43e98263197ea348d8ea690
Sonya <sofaahramovic0@gmail.com>
2024-05-15 19:24:08 +0300

    1

--- pages/mainPage/mainPage.js
+++ pages/mainPage/mainPage.js
@@ -1,147 +1,111 @@
 import React from 'react';
+import mainPage from './mainPage.module.css';
+import map from "./mainPages.images/backheader.png";
+import dom from "./mainPages.images/dom.png";
+import tg from "./mainPages.images/tg.png";
+import inst from "./mainPages.images/inst.png";
+import minsk from "./mainPages.images/minsk.png";
+import brest from "./mainPages.images/brest.png";
 
-const mainPage = () => {
-    return <div>
-<body>
-<header>
-<div classname="nav" id="nav">
+const MainPage = () => {
+    return (
+        <div>
+            <div className={mainPage.nav}>
+                <div className={mainPage.menu}>
+                    <ul>
+                        <li>ВОЗМОЖНОСТИ</li>
+                        <li>T & T</li>
+                        <li>ПОПУЛЯРНОЕ</li>
+                    </ul>
+                    <div className={mainPage.buttons}>
+                        <center>
+                            <button id="signupButton">
+                                <p>РЕГИСТРАЦИЯ</p>
+                            </button>
+                            <button id="signinButton">
+                                <p>ВХОД В АККАУНТ</p>
+                            </button>
+                        </center>
+                    </div>
+                </div>
 
-	<div classname="menu">
-	<ul>
-		<li><a href="#possib">ВОЗМОЖНОСТИ</a></li>
-		<li><a id="first"><b>T&T</b></a></li>
-		<li><a href="#info">ПОПУЛЯРНОЕ</a></li>
-	</ul>
-	</div>
+                <div>
+                    <div className={mainPage.text}>
+                        <p id="t4"><b>ВОЗМОЖНОСТИ</b></p>
+                        <p>Мы предлагаем большой спектр</p>
+                        <p>функций, которые вам доступны, а</p>
+                        <p>именно</p>
+                    </div>
+                    <div className={mainPage.blocks}>
+                        <div className={mainPage.poss1}>
+                            <center>
+                                <img src={dom}/>
+                                <p id="t5"><b>Просмотр</b></p>
+                                <p id="t6"><b>достопримечательностей</b></p>
+                                <p>Вы можете выбрать любую</p>
+                                <p>страну или город на свой вкус,</p>
+                                <p>просмотреть ее знаменитые</p>
+                                <p>памятники архитектуры, музеи и</p>
+                                <p id="t7">так далее</p>
+                            </center>
+                        </div>
+                        <div className={mainPage.poss2}>
+                            <center>
+                                <img src={map}/>
+                                <p id="t5"><b>Составление</b></p>
+                                <p id="t5"><b>туристических</b></p>
+                                <p><b>маршрутов</b></p>
+                                <p>Вы можете выбрать любые</p>
+                                <p>достопримечательности из списка и</p>
+                                <p>составить из них свой маршрут</p>
+                                <p>памятники архитектуры, музеи и</p>
+                                <p>самостоятельно</p>
+                            </center>
+                        </div>
+                        <div className={mainPage.poss3}>
+                            <center>
+                                <p id="t5"><b>Визуализация</b></p>
+                                <p id="t6"><b>маршрутов на карте</b></p>
+                                <p>Вы можете пользоваться</p>
+                                <p>Яндекс.Картами для того, чтобы</p>
+                                <p>визуализировать свой маршрут и</p>
+                                <p>ставить на нем метки</p>
+                            </center>
+                        </div>
+                    </div>
+                </div>
 
-	<div classname="nextmenu">
-	<b>
-		<a id="t1">ПЛАНИРУЙ</a><br>
-		<a id="t2">СОЗДАВАЙ</a><br>
-		<a id="t3">ПУТЕШЕСТВУЙ</a>
-	</b>
-	</div>
+                <div className={mainPage.info}>
+                    <div className={mainPage.text1}>
+                        <p>ПОПУЛЯРНЫЕ ГОРОДА</p>
+                        <p>Мы собрали список самых популярных городов и достопримечательностей Беларуси для ваших
+                            незабываемых путешествий</p>
+                        <div className={mainPage.blocks1}>
+                            <div className={mainPage.city1}>
+                                <img src={minsk}/>
+                                <p>Минск</p>
+                            </div>
+                            <div className={mainPage.city2}>
+                                <img src={brest}/>
+                                <p>Брест</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-	<div classname="buttons">
-	<center>
-		<button id="signupButton">
-		<a>РЕГИСТРАЦИЯ</a>
-		</button>
-		<br>
-		<button id="signinButton">
-		<a>ВХОД В АККАУНТ</a>
-		</button>
-	</center>
-	</div>
-</div>
-</header>
+                <hr/>
+                <footer>
+                    <div className={mainPage.logo}>
+                        <p>T & T</p>
+                    </div>
+                    <div className={mainPage.links}>
+                        <img src={tg}/>
+                        <img src={inst}/>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    );
+}
 
-<div classname="possib" id="possib">
-	<div classname="text">
-		<a id="t4"><b>ВОЗМОЖНОСТИ</b></a>
-		<a>Мы предлагаем большой спектр</a>
-		<a>функций, которые вам доступны, а</a>
-		<a>именно</a>
-	</div>
-	<div classname="blocks">
-		<div classname="poss1">
-		<center>
-			<img src="{% static 'img/dom.png' %}" width="30%">
-			<a id="t5"><b>Просмотр</b></a>
-			<a id="t6"><b>достопримечательностей</b></a>
-			<a>Вы можете выбрать любую</a>
-			<a>страну или город на свой вкус,</a>
-			<a>просмотреть ее знаменитые</a>
-			<a>памятники архитектуры, музеи и</a>
-			<a id="t7">так далее</a>
-		</center>
-		</div>
-
-		<div classname="poss2">
-		<center>
-			<img src="{% static 'img/row.png' %}" width="30%">
-			<a id="t5"><b>Составление</b></a>
-			<a id="t5"><b>туристических</b></a>
-			<a id="t6"><b>маршрутов</b></a>
-			<a>Вы можете выбрать любые</a>
-			<a>достопримечательности из списка и</a>
-			<a>составить из низ свой маршрут</a>
-			<a>памятники архитектуры, музеи и</a>
-			<a id="t7">самостоятельно</a>
-		</center>
-		</div>
-
-		<div classname="poss3">
-		<center>
-			<img src="{% static 'img/map.png' %}" width="30%">
-			<a id="t5"><b>Визуализация</b></a>
-			<a id="t6"><b>маршрутов на карте</b></a>
-			<a>Вы можете пользоваться</a>
-			<a>Яндекс. Картами для того, чтобы</a>
-			<a>визуализировать свой маршрут и</a>
-			<a id="t7">ставить на нем метки</a>
-		</center>
-		</div>
-	</div>
-</div>
-
-<div classname="info" id="info">
-	<div classname="text1">
-		<a id="t4"><b>ПОПУЛЯРНЫЕ<br>ГОРОДА</b></a>
-		<a>Мы собрали список самых популярных<br>
-		городов и достопримечательностей<br>
-		Беларуси для выших незабываемых<br>
-		путешествий</a>
-
-		<div classname="blocks1">
-			<div classname="city1">
-				<img src="{% static 'img/minsk.png' %}" width="100%">
-				<a>Минск</a>
-			</div>
-
-			<div classname="city2">
-				<img src="{% static 'img/brest.png' %}" width="100%">
-				<a>Брест</a>
-			</div>
-
-			<div classname="city3">
-				<img src="{% static 'img/mogilev.png' %}" width="100%">
-				<a>Могилев</a>
-			</div>
-		</div>
-
-		<div classname="blocks2">
-			<div classname="city4">
-				<img src="{% static 'img/Gomel.png' %}" width="100%">
-				<a>Гомель</a>
-			</div>
-
-			<div classname="city5">
-				<img src="{% static 'img/grodno.png' %}" width="100%">
-				<a>Гродно</a>
-			</div>
-
-			<div classname="city6">
-				<img src="{% static 'img/pinsk.png' %}" width="100%">
-				<a>Пинск</a>
-			</div>
-		</div>
-	</div>
-</div>
-<hr>
-<footer>
-	<div classname="logo">
-		<a href="#nav">T&T</a>
-	</div>
-
-	<div classname="links">
-		<img src="{% static 'img/inst.png' %}" width="3%">
-		<img src="{% static 'img/tg.png' %}" id="tg" width="3%">
-	</div>
-</footer>
-
-</body>
- </div>;
-};
-
-export default mainPage;
+export default MainPage;
--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import mainPage from "./pages/mainPage.js";
+import MainPage from "../pages/mainPage/mainPage";
 
 function App(){
 return(
   <div>
     <Router>
        <Routes>
-         <Route path="/" elements=element={<mainPage/>}></Route>
+         <Route path="/" elements=element={<MainPage/>}></Route>
        </Routes>
     </Router>
   </div>
 );
 }
+export default App;
 
 
